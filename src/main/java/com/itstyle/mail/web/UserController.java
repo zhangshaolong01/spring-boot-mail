2f8937611f8858abf550759137ce7ef0578745c4
zhangshaolong <zhangshaolong@windows10.microdone.cn>
2019-06-24 10:36:04 +0800

    1

--- src/main/java/com/itstyle/mail/web/UserController.java
+++ src/main/java/com/itstyle/mail/web/UserController.java
@@ -8,6 +8,9 @@
  */
 package com.itstyle.mail.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,8 @@ import com.itstyle.mail.entity.User;
 import com.itstyle.mail.service.IUserService;
 
 import io.swagger.annotations.Api;
+import net.sf.json.JSONArray;
+import net.sf.json.JsonConfig;
 
 /**   
 * Copyright: Copyright (c) 2018 LanRu-Caifu
@@ -50,5 +55,33 @@ public class UserController {
 	@PostMapping("add")
 	public Result add(User user) {
 		return userService.addUser(user);
+	}
+	
+	/**
+	 * 订单信息上传
+	 * @param user
+	 * @return
+	 */
+	@PostMapping("uploadOderIInfo")
+	public Result uploadOderIInfo(String data) {
+		System.out.println(data);
+		Result result = new Result();
+		return result.ok();
+	}
+	
+	/**
+	 * 服务执行记录上传
+	 * @param user
+	 * @return
+	 */
+	@PostMapping("uploadExeServiceRecord")
+	public Map uploadExeServiceRecord(String data) {
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.setRootClass(Map.class);
+		JSONArray fromObject = JSONArray.fromObject(data,jsonConfig);
+		System.out.println(fromObject.toString());
+		Map<String,Object> map = new HashMap<>();
+		map.put("businessCode", "0000");
+		return map;
 	}
 }
